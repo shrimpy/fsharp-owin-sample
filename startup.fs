375e47d8f31238eb6e7f1740152c806a096f8ac6
Konstantin Tarkus <hello@tarkus.me>
2014-06-05 13:37:35 +0400

    Initial commit

---
+++ startup.fs
@@ -0,0 +1,14 @@
+﻿namespace Server
+
+open Owin
+open Microsoft.Owin
+
+type Startup() =
+    member x.Configuration(app: Owin.IAppBuilder) =
+        app.Run(fun ctx ->
+            ctx.Response.ContentType <- "text/plain"
+            ctx.Response.WriteAsync("Owin Sample with F#")
+        ) |> ignore
+
+[<assembly: OwinStartup(typeof<Startup>)>]
+do ()
